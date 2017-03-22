efcbcdb3db8164dff7a3ac9787c6851da547b021
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-22 20:35:17 -0300

    Adicionado font personalizada com typeface e gif de carregamento com glide

--- app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/MainActivity.java
@@ -1,13 +1,31 @@
 package br.com.fiap.a2tina_android_ex05_navigationdrawerfragments;
 
+import android.graphics.Typeface;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtMyApp;
+    ImageView gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gif = (ImageView)findViewById(R.id.gif);
+        txtMyApp = (TextView)findViewById(R.id.myapp);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/kaushan.ttf");
+        txtMyApp.setTypeface(font);
+
+        Glide.with(this)
+                .load(R.drawable.gif1) // aqui é teu gif
+                .asGif()
+                .into(gif);
     }
 }
