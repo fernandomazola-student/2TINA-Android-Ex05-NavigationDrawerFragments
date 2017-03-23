814a6180cca528f2977a1e01976e344424683221
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-23 17:25:47 -0300

    Adicionado os componentes navigationview e toggle

--- app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/CarroActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/CarroActivity.java
@@ -1,5 +1,7 @@
 package br.com.fiap.a2tina_android_ex05_navigationdrawerfragments;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,13 +9,19 @@ import android.support.v7.widget.Toolbar;
 public class CarroActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carro);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout)findViewById(R.id.activity_carro);
         setSupportActionBar(toolbar);
 
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.abrir, R.string.fechar);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 }
