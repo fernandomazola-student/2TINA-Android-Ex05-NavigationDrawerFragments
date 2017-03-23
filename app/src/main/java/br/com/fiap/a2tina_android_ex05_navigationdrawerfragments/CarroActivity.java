ba3d17e4c1edaf911c819cf9c24d21830b19fbfd
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-23 14:31:27 -0300

    Adicionado activity carros e estilizando

--- app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/CarroActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/CarroActivity.java
@@ -2,12 +2,18 @@ package br.com.fiap.a2tina_android_ex05_navigationdrawerfragments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CarroActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carro);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
