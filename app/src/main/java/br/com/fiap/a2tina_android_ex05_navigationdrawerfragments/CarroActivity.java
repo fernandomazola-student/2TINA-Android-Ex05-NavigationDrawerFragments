8e4a80b3acfe7791caefee50e9bdf31e8921baac
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-24 11:40:50 -0300

    Adicionado arquivos fragments e criado evento navitagion item selected

--- app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/CarroActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_ex05_navigationdrawerfragments/CarroActivity.java
@@ -1,16 +1,28 @@
 package br.com.fiap.a2tina_android_ex05_navigationdrawerfragments;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import layout.CadilacFragment;
+import layout.ImpalaFragment;
 
 public class CarroActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +30,36 @@ public class CarroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_carro);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         drawerLayout = (DrawerLayout)findViewById(R.id.activity_carro);
+        fragmentContainer = (FrameLayout)findViewById(R.id.fragment_container);
+
         setSupportActionBar(toolbar);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.abrir, R.string.fechar);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+
+        navigationView = (NavigationView)findViewById(R.id.navigationview);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.mnImpala){
+                    Toast.makeText(CarroActivity.this, "Sobre", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
     }
+
+
+
+
+//    public void abrirFragment(Fragment fragment){
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).addToBackStack(null).commit();
+//    }
+//
+//    public void CarroImpala(View view){
+//           getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ImpalaFragment()).addToBackStack(null).commit();
+//   }
 }
